c4f34830c4733f153c15f113f36f4c396aeca954
unknown <gdias.bruno@gmail.com>
2020-07-28 12:23:39 -0300

    galeria terminada

--- script.js
+++ script.js
@@ -55,9 +55,5 @@ window.onload=function(){
   check();
 }
 
-$(function () {
-  $('.pop').on('click', function () {
-    $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-    $('#imagemodal').modal('show');
-  });
-});
+
+
